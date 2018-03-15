b6508f0a955d131b9ecf612713907dc13b00cf48
Júlio César <julio.souzam@gmail.com>
2018-03-15 18:02:13 -0300

    get text writed in input text

--- app/src/main/java/me/ojuliocesar/kotlinlang/kotlinapp/MainActivity.kt
+++ app/src/main/java/me/ojuliocesar/kotlinlang/kotlinapp/MainActivity.kt
@@ -2,6 +2,7 @@ package me.ojuliocesar.kotlinlang.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var enterEmail = enterEmail.text
+
+        btnShow.setOnClickListener{ resultText }
 
 
     }
