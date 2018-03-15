ac6438fd9c70e90531f9c09df1201c60ae409005
Júlio César <julio.souzam@gmail.com>
2018-03-15 14:29:32 -0300

    first commit

---
+++ app/src/main/java/me/ojuliocesar/kotlinlang/kotlinapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package me.ojuliocesar.kotlinlang.kotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
