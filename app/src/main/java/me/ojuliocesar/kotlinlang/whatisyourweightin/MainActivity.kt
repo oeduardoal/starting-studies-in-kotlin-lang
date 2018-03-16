b19393b49832a2ba1a2b6b6b4187bc1139511751
Júlio César <julio.souzam@gmail.com>
2018-03-16 11:23:48 -0300

    finished... for now

--- app/src/main/java/me/ojuliocesar/kotlinlang/whatisyourweightin/MainActivity.kt
+++ app/src/main/java/me/ojuliocesar/kotlinlang/whatisyourweightin/MainActivity.kt
@@ -2,13 +2,36 @@ package me.ojuliocesar.kotlinlang.whatisyourweightin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var marsG = 0.38
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //show our magic
+        var weight = enterWeight.text;
+
+        //Get on listiner btn
+        showWeightBtn.setOnClickListener {
+
+            var result = calculateWeight(weight.toString().toDouble())
+
+            resultView.text = "You weight " + result.toString() + " on Mars"
+
+
+        }
+
+    }
+
+
+    fun calculateWeight(userWeight: Double): Double { // 89.78
+
+        return userWeight * marsG
 
     }
 }
