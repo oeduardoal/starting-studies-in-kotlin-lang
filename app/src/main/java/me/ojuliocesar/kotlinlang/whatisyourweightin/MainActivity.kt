7524ea2cad7f3799a7a6d6aadf218946958bb222
Júlio César <julio.souzam@gmail.com>
2018-03-16 17:57:41 -0300

    checkbox clickable mars, venus and jupiter

--- app/src/main/java/me/ojuliocesar/kotlinlang/whatisyourweightin/MainActivity.kt
+++ app/src/main/java/me/ojuliocesar/kotlinlang/whatisyourweightin/MainActivity.kt
@@ -2,11 +2,15 @@ package me.ojuliocesar.kotlinlang.whatisyourweightin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    var marsG = 0.38
+    val marsG = 0.38
+    val venusG = 0.91
+    val jupiterG = 2.34
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,21 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        marsCheckbox.setOnClickListener(this) // registering our clickListener
+        venusCheckbox.setOnClickListener(this)
+        jupiterCheckbox.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        v as CheckBox
+        var isChecked: Boolean = v.isChecked
+
+        when(v.id){
+            R.id.marsCheckbox -> if (isChecked) { resultView.text = "Mars checked" } else { resultView.text = "Mars unchecked"  }
+            R.id.venusCheckbox -> if (isChecked) { resultView.text = "Venus checked" } else { resultView.text = "Venus unchecked"  }
+            R.id.jupiterCheckbox -> if (isChecked) { resultView.text = "Jupiter checked" } else { resultView.text = "Jupiter unchecked"  }
+        }
     }
 
 
