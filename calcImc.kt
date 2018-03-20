a6e1e9181966fc9f7d1d06b34a5e9ab31ebbf3f3
Júlio César <julio.souzam@gmail.com>
2018-03-20 09:52:53 -0300

    calculate imc

---
+++ calcImc.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    var kg, altura, imc
+
+    println("Digite seu peso")
+    kg = readLine()
+
+    println("Digite sua altura")
+    altura = readLine()
+
+
+    imc = kg / altura * altura
+
+}
